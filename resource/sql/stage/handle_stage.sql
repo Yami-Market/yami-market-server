b047c1e468bf3eb6414d2d3d3369989f1fe76200
Zhen Jun Xu <me@arlenx.dev>
2022-12-02 03:16:47 -0500

    chore: add category view

--- resource/sql/stage/handle_stage.sql
+++ resource/sql/stage/handle_stage.sql
@@ -1,23 +1,23 @@
 -- Active: 1669593277665@@127.0.0.1@5432@yami-market@public
 -- category table
-INSERT INTO category (id, name)
-SELECT DISTINCT category_id, category_name
+INSERT INTO category (id, name, parent_id)
+SELECT DISTINCT category_id, category_name, NULL
 FROM stage;
 
 -- second_category table
 
-INSERT INTO second_category (id, name, category_id)
+INSERT INTO category (id, name, parent_id)
 SELECT DISTINCT second_category_id, second_category_name, category_id
 FROM stage;
 
 -- third_category table
 
-INSERT INTO third_category (id, name, second_category_id)
+INSERT INTO category (id, name, parent_id)
 SELECT DISTINCT third_category_id, third_category_name, second_category_id
 FROM stage;
 
 -- product table
 
-INSERT INTO product (id, name, unit_price, image_url, third_category_id)
+INSERT INTO product (id, name, list_price, image_url, category_id)
 SELECT DISTINCT product_id, product_name, unit_price, image_url, third_category_id
 FROM stage;
