25f2f022f0efe968f53fa73b18d800348509eb92
Zhen Jun Xu <me@arlenx.dev>
2022-12-02 18:41:56 -0500

    chore: categoty sql

--- resource/sql/sample/category_sample.sql
+++ resource/sql/sample/category_sample.sql
@@ -18,6 +18,19 @@ WITH RECURSIVE category_tree AS
                     UNION ALL
                     SELECT c.id, c.name, c.parent_id, ct.level + 1
                     FROM category c
+                             INNER JOIN category_tree ct ON c.parent_id = ct.id)
+SELECT ct.id AS id, ct.name AS name, ct.parent_id AS parent_id, c.name AS parent_name, level
+FROM category_tree ct
+         LEFT JOIN category c ON ct.parent_id = c.id;
+
+-- Sample 3
+WITH RECURSIVE category_tree AS
+                   (SELECT id, name, parent_id, 1::integer level
+                    FROM category
+                    WHERE parent_id IS NULL
+                    UNION ALL
+                    SELECT c.id, c.name, c.parent_id, ct.level + 1
+                    FROM category c
                              JOIN category_tree ct ON c.parent_id = ct.id)
 SELECT *
 FROM category_tree ct
@@ -25,7 +38,7 @@ FROM category_tree ct
          INNER JOIN category c2 ON c.parent_id = c2.id
 WHERE ct.level = 3;
 
--- Sample 3
+-- Sample 4
 WITH RECURSIVE category_tree AS
                    (SELECT id, name, parent_id, 1::integer level
                     FROM category
--- resource/sql/view/category_view.sql
+++ resource/sql/view/category_view.sql
@@ -1,9 +1,14 @@
 -- Category Recursive View
-CREATE OR REPLACE RECURSIVE VIEW category_recursive_view (id, name, parent_id, level) AS
-SELECT id, name, parent_id, 1::integer recursive_level
-FROM category
-WHERE parent_id IS NULL
-UNION ALL
-SELECT c.id, c.name, c.parent_id, ct.level + 1
-FROM category c
-         INNER JOIN category_recursive_view ct ON c.parent_id = ct.id
+CREATE OR REPLACE VIEW category_recursive_view
+AS
+WITH RECURSIVE category_tree AS
+                   (SELECT id, name, parent_id, 1::integer level
+                    FROM category
+                    WHERE parent_id IS NULL
+                    UNION ALL
+                    SELECT c.id, c.name, c.parent_id, ct.level + 1
+                    FROM category c
+                             INNER JOIN category_tree ct ON c.parent_id = ct.id)
+SELECT ct.id AS id, ct.name AS name, ct.parent_id AS parent_id, c.name AS parent_name, level
+FROM category_tree ct
+         LEFT JOIN category c ON ct.parent_id = c.id;
