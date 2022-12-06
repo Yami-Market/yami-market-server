98f05a3a4dfa5029ff5fcf393265747c59f060bf
Zhen Jun Xu <me@arlenx.dev>
2022-12-06 16:37:11 -0500

    refactor: remove log

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,6 +1,5 @@
-from flask import Blueprint
-from flask import current_app as app
-from flask import jsonify
+# from flask import current_app as app
+from flask import Blueprint, jsonify
 
 from app.services.category_service import get_category_list
 
@@ -10,5 +9,5 @@ bp = Blueprint(name='category', import_name=__name__, url_prefix='/v1')
 @bp.get('/category')
 def get_category():
     category_list = get_category_list()
-    app.logger.debug(category_list)
+    # app.logger.debug(category_list)
     return jsonify(category_list.dict()), 200
