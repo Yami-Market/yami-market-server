76bae15f021926653767aa9b47c5dfe7c75fa17c
Zhen Jun Xu <me@arlenx.dev>
2022-12-08 09:52:06 -0500

    chore: add db url

--- db/__init__.py
+++ db/__init__.py
@@ -9,6 +9,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DB_HOST is None:
     raise ValueError('DB_HOST is not set')
@@ -20,7 +21,8 @@ if DB_PASSWORD is None:
     raise ValueError('DB_PASSWORD is not set')
 
 pool = ConnectionPool(
-    f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}',
+    DATABASE_URL
+    or 'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}',
     min_size=1,
     max_size=20,
 )
