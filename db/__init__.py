3af3c75db7182d434d898b16d4d52df630a7a24f
Zhen Jun Xu <me@arlenx.dev>
2022-12-08 09:58:04 -0500

    chore: update db config

--- db/__init__.py
+++ db/__init__.py
@@ -11,14 +11,15 @@ DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-if DB_HOST is None:
-    raise ValueError('DB_HOST is not set')
-if DB_NAME is None:
-    raise ValueError('DB_NAME is not set')
-if DB_USERNAME is None:
-    raise ValueError('DB_USERNAME is not set')
-if DB_PASSWORD is None:
-    raise ValueError('DB_PASSWORD is not set')
+if DATABASE_URL is None:
+    if DB_HOST is None:
+        raise ValueError('DB_HOST is not set')
+    if DB_NAME is None:
+        raise ValueError('DB_NAME is not set')
+    if DB_USERNAME is None:
+        raise ValueError('DB_USERNAME is not set')
+    if DB_PASSWORD is None:
+        raise ValueError('DB_PASSWORD is not set')
 
 pool = ConnectionPool(
     DATABASE_URL
